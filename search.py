d285e3063b24600633e67321295224ed6ecee436
ElTiburon <tiburonsokol@gmail.com>
2021-05-27 00:30:19 +0300

    Создана функция определения расстояния

--- search.py
+++ search.py
@@ -1,23 +1,28 @@
 import math
 from citysearch import citySearch
+class calculate:
+    def __init__(self, la1=None, lo1=None, la2=None, lo2=None, res=None):
+        self.la1 = la1
+        self.lo1 = lo1
+        self.la2 = la2
+        self.lo2 = lo2
+        self.res = res
 
-def haversine(lat1, lon1, lat2, lon2):
-    # расстояние между широтой и долготой
-    dLat = (lat2 - lat1) * math.pi / 180.0
-    dLon = (lon2 - lon1) * math.pi / 180.0
-    # преобразовать в радианы
-    lat1 = (lat1) * math.pi / 180.0
-    lat2 = (lat2) * math.pi / 180.0
-    a = (pow(math.sin(dLat / 2), 2) +
+        dLat = (la2 - la1) * math.pi / 180.0
+        dLon = (lo2 - lo1) * math.pi / 180.0
+        la1 = (la1) * math.pi / 180.0
+        la2 = (la2) * math.pi / 180.0
+        a = (pow(math.sin(dLat / 2), 2) +
 
-         pow(math.sin(dLon / 2), 2) *
+             pow(math.sin(dLon / 2), 2) *
 
-         math.cos(lat1) * math.cos(lat2));
+             math.cos(la1) * math.cos(la2));
+        rad = 6371
+        c = 2 * math.asin(math.sqrt(a))
+        res = rad * c
+        self.res = res
 
-    rad = 6371 #радиус земли
-    c = 2 * math.asin(math.sqrt(a))
-    return rad * c
-# Код теста погрешность 3-20 км (внутри города, 2000 км)
+
 if __name__ == "__main__":
     lat1 = 59.938732
     lon1 = 30.316229
@@ -25,4 +30,4 @@ if __name__ == "__main__":
     lon2 = 39.018187000000000
     city1=citySearch(lat=lat1,lon=lon1)
     city2=citySearch(lat=lat2, lon=lon2)
-    print(round(haversine(lat1, lon1, lat2, lon2),2), "км. ",' между ',city1.city,' и ',city2.city)
+    print(round(calculate(la1=lat1, lo1=lon1, la2=lat2, lo2=lon2).res,2), "км. ",' между ',city1.city,' и ',city2.city)
